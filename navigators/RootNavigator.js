430540ed224c3f68f22d3a7a4b202316091f775a
vetacode <vetacode@gmail.com>
2025-10-28 10:49:20 +0700

    adding root navigator component

--- navigators/RootNavigator.js
+++ navigators/RootNavigator.js
@@ -7,8 +7,9 @@ const Stack = createNativeStackNavigator();
 
 const RootNavigator = () => {
   return (
-    <Stack.Navigator>
-      {/* Set up stack navigation to move between welcome screen and subscribe screen here */}
+    <Stack.Navigator screenOptions={{ headerStyle: { backgroundColor: '#F4CE14' } }}>
+      <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      <Stack.Screen name="Subscribe" component={SubscribeScreen} />
     </Stack.Navigator>
   );
 };
